00b4e49b31de392ce9fa45efdb2ab936c220afa7
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2022-01-11 10:43:14 +0200

    fix

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2';
+const BASE_URL = 'https://restcountries.com/v2';
 
 // function fetchCountryByName(countryName) {
 //   return fetch(`${BASE_URL}/name/${countryName}`).then(response => {
